958f2b4d8a827a6ed6fa29f9f5892c9a50a9889c
Kenneth Martens <kenneth.martens93@gmail.com>
2022-07-09 15:40:41 -0500

    Adds a profile for ghpages and changes production base urls

---
+++ src/environments/environment.ghpages.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    assetsBasePath: '/photography-website/assets/',
+    map: {
+        mapImplementationUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+};
