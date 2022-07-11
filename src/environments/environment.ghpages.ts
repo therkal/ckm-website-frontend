dacd1dd01b61a46801b32aba62f1886774fbb22e
Kenneth Martens <kenneth.martens93@gmail.com>
2022-07-11 09:35:17 -0500

    Fix ghpages deploy after rename of repo

--- src/environments/environment.ghpages.ts
+++ src/environments/environment.ghpages.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    assetsBasePath: '/photography-website/assets/',
+    assetsBasePath: '/ckm-website-frontend/assets/',
     map: {
         mapImplementationUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
